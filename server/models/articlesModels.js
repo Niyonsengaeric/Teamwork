8882248276d2356cbf2a325e66c29d1d059461cc
Believe <niyeric11@.com>
2019-09-12 00:06:24 +0200

    feature(API): edit article [finishes #168432075]

--- server/models/articlesModels.js
+++ server/models/articlesModels.js
@@ -2,21 +2,21 @@ const articles = [
 
   {
     articleId: 1,
+    createdOn: '2019-09-11T10:32:32+02:00',
     title: 'Identity-based habits',
     authorId: 2,
     authorName: 'BYUSA PRINCE DACY',
     article: 'One of the central ideas in the book is the concept of building “identity-based habits”, which essentially recommends focusing on the type of person you wish to become rather than the outcome you wish to achieve. One reader named Roland used the idea to improve his eating habits. “I stopped eating unhealthy food via identity change,” he wrote. “I tried many times in the past, but it became easy — natural — only after I had made the conscious decision that I want to be someone who eats healthy. Instead of aiming for I want to stop eating bad food, I tried changing the mindset to I am someone that eats healthy and lives a healthy life. It changes how you approach things.',
 
-    createdOn: '$2b$10$lcLHDlw0YPQ1nLDbodynveS/yx6K.SamH6TwzalYEHoijm1W0jouu',
   },
   {
     articleId: 2,
+    createdOn: '2019-04-12T23:51:56+02:00',
     title: 'Growth Mindset',
     authorId: 3,
     authorName: 'SARPONG BENITH',
     article: " I can’t do it, I can’t learn this, it's too hard, Come on, I’m just a Junior Developer, WebPack is not my thing, I tried it and failed.Those are the words of a developer drenched and soaked with imposter syndrome.Over time we keep fighting ourselves, trying to run away from our shadows, trying to find shortcuts, yes I did also, I really did: (At a point in my life, I just felt some things were not meant for me, I felt maths was too complex for me, JavaScript was not my thing!!",
 
-    createdOn: '$2b$10$lcLHDlw0YPQ1nLDbodynveS/yx6K.SamH6TwzalYEHoijm1W0jouu',
   },
 
 ];
--- server/routes/articles.js
+++ server/routes/articles.js
@@ -2,7 +2,8 @@ import express from 'express';
 import auth from '../middlewares/checkAuth';
 import articlesController from '../controllers/articlesControllers';
 
-const { newArticle } = articlesController;
+const { newArticle, editArticle } = articlesController;
 const router = express.Router();
 router.post('/articles', auth, newArticle);
+router.patch('/articles/:id', auth, editArticle);
 export default router;
