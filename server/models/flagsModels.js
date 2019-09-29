4b39db75c21eb6dc65920efa867026b7bb53f417
NIYONSENGA ERIC <50768983+Niyonsengaeric@users.noreply.github.com>
2019-09-29 04:47:59 +0000

    bug(API+UI): fixing bugs (#29)
    [finishes #168823860]

--- server/models/flagsModels.js
+++ server/models/flagsModels.js
@@ -12,7 +12,7 @@ const flags = [
     flagId: 2,
     type: 'article',
     flagedId: 2,
-    content: "I can’t do it, I can’t learn this, it's too hard, Come on, I’m just a Junior Developer, WebPack is not my thing, I tried it and failed.Those are the words of a developer drenched and soaked with imposter syndrome.Over time we keep fighting ourselves, trying to run away from our shadows, trying to find shortcuts, yes I did also, I really did: (At a point in my life, I just felt some things were not meant for me, I felt maths was too complex for me, JavaScript was not my thing!!",
+    content: "It can’t be like that",
     reason: 'lies',
 
 
