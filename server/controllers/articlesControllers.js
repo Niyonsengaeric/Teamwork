201f14dd2f1047df15bca4ba34266e2beba50186
NIYONSENGA ERIC <50768983+Niyonsengaeric@users.noreply.github.com>
2019-10-02 20:47:40 +0000

    Refactoring API (#34)

--- server/controllers/articlesControllers.js
+++ server/controllers/articlesControllers.js
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import flags from '../models/flagsModels';
 import validate from '../middlewares/validateArticles';
 import validateComment from '../middlewares/validatecomment';
 import articles from '../models/articlesModels';
@@ -7,202 +8,218 @@ import users from '../models/usersModels';
 import response from '../helpers/response';
 
 class articlescontrolllers {
-  // create new article
   static async newArticle(req, res) {
-    const { error } = validate(req.body);
-    if (error) { return response.response(res, 422, 'error', `${error.details[0].message}`, true); }
-    const { title, article } = req.body;
-    const { id, isAdmin } = req.user;
-    if (isAdmin) { response.response(res, 403, 'error', 'not allowed for Administrator to create Articles', true); } else {
-      const getuser = users.find((finduser) => finduser.id === id);
-      const { id: authorId, firstName, lastName } = getuser;
+    try {
+      const { error } = validate(req.body);
+      if (error) { return response.response(res, 422, 'error', `${error.details[0].message}`, true); }
+      const { title, article } = req.body;
+      const { id, isAdmin } = req.user;
+      if (isAdmin) { response.response(res, 403, 'error', 'not allowed for Administrator to create Articles', true); } else {
+        const getuser = users.find((finduser) => finduser.id === id);
+        const { id: authorId, firstName, lastName } = getuser;
 
-      const checkexisting = await articles.filter(
-        (regArticles) => regArticles.title === req.body.title && regArticles.authorId === id,
-      );
-
-      // console.log(checkexisting[0]);
-      if (checkexisting.length > 0) {
-        response.response(res, 409, 'error', ` Article arleady registered with An ID Of :  ${checkexisting[0].articleId} `, true);
-      } else {
-        const authorName = `${firstName} ${lastName}`;
-        const addarticle = {
-          articleId: articles.length + 1,
-          createdOn: moment().format(),
-          title,
-          authorId,
-          authorName,
-          article,
-        };
-        articles.push(addarticle);
-        const data = { ...addarticle };
-        delete data.authorId;
-        response.response(res, 201, 'Articles created successfully', data, false);
+        const checkexisting = await articles.filter(
+          (regArticles) => regArticles.title === req.body.title && regArticles.authorId === id,
+        );
+        if (checkexisting.length > 0) {
+          response.response(res, 409, 'error', ` Article arleady registered with An ID Of :  ${checkexisting[0].articleId} `, true);
+        } else {
+          const authorName = `${firstName} ${lastName}`;
+          const addarticle = {
+            articleId: articles.length + 1,
+            createdOn: moment().format(),
+            title,
+            authorId,
+            authorName,
+            article,
+          };
+          articles.push(addarticle);
+          const data = { ...addarticle };
+          delete data.authorId;
+          response.response(res, 201, 'Articles created successfully', data, false);
+        }
       }
+
+      return (article);
+    } catch (error) {
+      return error;
     }
-
-    return (article);
   }
-
-  // editarticle
 
   static async editArticle(req, res) {
-    const { error } = validate(req.body);
-    if (error) { return response.response(res, 422, 'error', `${error.details[0].message}`, true); }
-    const { id: userId } = req.user;
-    const { title, article } = req.body;
-    const { id } = req.params;
+    try {
+      const { error } = validate(req.body);
+      if (error) { return response.response(res, 422, 'error', `${error.details[0].message}`, true); }
+      const { id: userId } = req.user;
+      const { title, article } = req.body;
+      const { id } = req.params;
 
-    // find already exist
-    const checkexisting = await articles.filter(
-      (regArticles) => regArticles.title === title
-        && regArticles.authorId === userId && regArticles.article === article,
-    );
-    // check if the user is the owner
-    const findArticleindex = articles.findIndex(
-      (findArticle) => findArticle.articleId === parseInt(id, 10)
-        && findArticle.authorId === userId,
-    );
-    if (findArticleindex !== -1) {
-      // check for already updated
-      if (checkexisting.length > 0) {
-        response.response(res, 409, 'error', 'Article arleady updated', true);
-      } else {
-        // update Article
-        articles[findArticleindex].title = title.trim();
-        articles[findArticleindex].article = article.trim();
-        return response.response(res, 200, 'article successfully edited”', articles[findArticleindex], false);
-      }
-    } else { response.response(res, 404, 'error', 'No article Found', true); }
+      const checkexisting = await articles.filter(
+        (regArticles) => regArticles.title === title
+          && regArticles.authorId === userId && regArticles.article === article,
+      );
+      const findArticleindex = articles.findIndex(
+        (findArticle) => findArticle.articleId === parseInt(id, 10)
+          && findArticle.authorId === userId,
+      );
+      if (findArticleindex !== -1) {
+        if (checkexisting.length > 0) {
+          response.response(res, 409, 'error', 'Article arleady updated', true);
+        } else {
+          articles[findArticleindex].title = title.trim();
+          articles[findArticleindex].article = article.trim();
+          return response.response(res, 200, 'article successfully edited”', articles[findArticleindex], false);
+        }
+      } else { response.response(res, 404, 'error', 'No article Found', true); }
 
 
-    return (articles);
+      return (articles);
+    } catch (error) {
+      return error;
+    }
   }
 
-  // delete article
   static async deleteArticle(req, res) {
-    const { id: userId, isAdmin } = req.user;
-    const { id } = req.params;
-    const findOwnerindex = await articles.findIndex(
-      (findArticle) => findArticle.articleId === parseInt(id, 10)
-        && findArticle.authorId === userId,
-    );
-    if (isAdmin) {
-      const findArticle = await articles.findIndex(
-        (findArticles) => findArticles.articleId === parseInt(id, 10),
+    try {
+      const { id: userId, isAdmin } = req.user;
+      const { id } = req.params;
+
+      const findOwnerindex = await articles.findIndex(
+        (findArticle) => findArticle.articleId === parseInt(id, 10)
+          && findArticle.authorId === userId,
       );
-      if (findArticle !== -1) {
-        articles.splice(findArticle, 1);
+      const findflag = await flags.findIndex(
+        (findflags) => findflags.flagedId === parseInt(id, 10) && findflags.type === 'article',
+      );
+
+      if (isAdmin) {
+        const findArticle = await articles.findIndex(
+          (findArticles) => findArticles.articleId === parseInt(id, 10),
+        );
+        if (findArticle !== -1) {
+          if (findflag === -1) {
+            return response.response(res, 403, 'error', 'Article not flagged  ', true);
+          }
+          articles.splice(findArticle, 1);
+          flags.splice(findflag, 1);
+          response.response(res, 200, 'article successfully deleted');
+        } else { return response.response(res, 404, 'error', 'article Not Found  ', true); }
+      } else if (findOwnerindex !== -1) {
+        articles.splice(findOwnerindex, 1);
         response.response(res, 200, 'article successfully deleted');
-      } else { return response.response(res, 404, 'error', 'article Not Found  ', true); }
-    } else if (findOwnerindex !== -1) {
-      // console.log(articles[findOwnerindex]);
-      articles.splice(findOwnerindex, 1);
-      response.response(res, 200, 'article successfully deleted');
-    } else {
-      return response.response(res, 404, 'error', 'article Not Found  ', true);
-    }
-    return (articles);
-  }
-
-  // comment on article
-  static async commentArticle(req, res) {
-    const { error } = validateComment(req.body);
-    if (error) {
-      return response.response(res, 422, 'error', `${error.details[0].message}`, true);
-    }
-
-    // destruct object
-    const { id: userId, isAdmin } = req.user;
-    const { id } = req.params;
-    const { comment } = req.body;
-
-    // limit admin to comment and allow for normal user
-    if (isAdmin) {
-      response.response(res, 401, 'error', 'not allowed for Administrator to comment on  Articles', true);
-    } else {
-      const checkArticle = await articles.findIndex(
-        (findArticle) => findArticle.articleId === parseInt(id, 10),
-      );
-      if (checkArticle !== -1) {
-        const { createdOn, title, article } = articles[checkArticle];
-        const addCommet = {
-          commentId: comments.length + 1,
-          articleId: parseInt(id, 10),
-          authorId: userId,
-          comment,
-        };
-        comments.push(addCommet);
-        const data = {
-          createdOn, title, article, comment,
-        };
-        response.response(res, 201, 'comment Added successfully', data, false);
       } else {
         return response.response(res, 404, 'error', 'article Not Found  ', true);
       }
+      return (articles);
+    } catch (error) {
+      return error;
     }
-    return (articles);
   }
 
-  // display All articles
-  static async getArticles(req, res) {
-    const data = [];
-    let j = 0;
-
-    if (articles.length === 0) {
-      return response.response(res, 404, 'error', 'no article Registered yet  ', true);
-    }
-    // get article starting from resent posted article
-
-    for (let i = articles.length - 1; i >= 0; i -= 1) {
-      // add data to new array;
-      data[j] = articles[i];
-      j += 1;
-    }
-    return response.response(res, 200, 'success', data, false);
-  }
-
-  // display specific article
-  static async specificArticle(req, res) {
-    const { id } = req.params;
-
-    const findArticle = articles.filter(
-      (specfarticles) => specfarticles.articleId === parseInt(id, 10),
-
-    );
-    if (findArticle.length > 0) {
-      const getcomments = await comments.filter(
-        (article) => article.articleId === parseInt(id, 10),
-      );
-      const {
-        articleId, createdOn, title, article, authorId,
-      } = findArticle[0];
-      const data = {
-        articleId,
-        createdOn,
-        title,
-        article,
-        authorId,
-        comments: getcomments,
-      };
-      response.response(res, 200, 'success', data, false);
-    } else { return response.response(res, 404, 'error', 'article Not Found  ', true); }
-    return (findArticle);
-  }
-
-  // find articles
-  static async filterArticle(req, res) {
-    if (req.query.articles) {
-      const { articles: article } = req.query;
-
-      const checktaget = articles.filter((regArticles) => regArticles.article.includes(`${article.trim()}`));
-      if (checktaget.length > 0) {
-        response.response(res, 200, 'success', checktaget, false);
-      } else {
-        return response.response(res, 404, 'error', `${article} don't match with any article`, true);
+  static async commentArticle(req, res) {
+    try {
+      const { error } = validateComment(req.body);
+      if (error) {
+        return response.response(res, 422, 'error', `${error.details[0].message}`, true);
       }
-    } else { return response.response(res, 405, 'error', 'please enter the tag', true); }
-    return (articles);
+
+      const { id: userId, isAdmin } = req.user;
+      const { id } = req.params;
+      const { comment } = req.body;
+
+      if (isAdmin) {
+        response.response(res, 401, 'error', 'not allowed for Administrator to comment on  Articles', true);
+      } else {
+        const checkArticle = await articles.findIndex(
+          (findArticle) => findArticle.articleId === parseInt(id, 10),
+        );
+        if (checkArticle !== -1) {
+          const { createdOn, title, article } = articles[checkArticle];
+          const addCommet = {
+            commentId: comments.length + 1,
+            articleId: parseInt(id, 10),
+            authorId: userId,
+            comment,
+          };
+          comments.push(addCommet);
+          const data = {
+            createdOn, title, article, comment,
+          };
+          response.response(res, 201, 'comment Added successfully', data, false);
+        } else {
+          return response.response(res, 404, 'error', 'article Not Found  ', true);
+        }
+      }
+      return (articles);
+    } catch (error) {
+      return error;
+    }
+  }
+
+  static async getArticles(req, res) {
+    try {
+      const data = [];
+      let j = 0;
+
+      if (articles.length === 0) {
+        return response.response(res, 404, 'error', 'no article Registered yet  ', true);
+      }
+      for (let i = articles.length - 1; i >= 0; i -= 1) {
+        data[j] = articles[i];
+        j += 1;
+      }
+      return response.response(res, 200, 'Liste of Articles', data, false);
+    } catch (error) {
+      return error;
+    }
+  }
+
+  static async specificArticle(req, res) {
+    try {
+      const { id } = req.params;
+
+      const findArticle = articles.filter(
+        (specfarticles) => specfarticles.articleId === parseInt(id, 10),
+      );
+      if (findArticle.length > 0) {
+        const getcomments = await comments.filter(
+          (article) => article.articleId === parseInt(id, 10),
+        );
+        const {
+          articleId, createdOn, title, article, authorId,
+        } = findArticle[0];
+        const data = {
+          articleId,
+          createdOn,
+          title,
+          article,
+          authorId,
+          comments: getcomments,
+        };
+        response.response(res, 200, 'Article successfully retrieved', data, false);
+      } else { return response.response(res, 404, 'error', 'article Not Found  ', true); }
+      return (findArticle);
+    } catch (error) {
+      return error;
+    }
+  }
+
+  static async filterArticle(req, res) {
+    try {
+      if (req.query.articles) {
+        const { articles: article } = req.query;
+
+        const checktaget = articles.filter((regArticles) => regArticles.article.includes(`${article.trim()}`));
+        if (checktaget.length > 0) {
+          response.response(res, 200, 'Article Found', checktaget, false);
+        } else {
+          return response.response(res, 404, 'error', `${article} don't match with any article`, true);
+        }
+      } else { return response.response(res, 405, 'error', 'please enter the tag', true); }
+      return (articles);
+    } catch (error) {
+      return error;
+    }
   }
 }
 
--- server/controllers/flagsControllers.js
+++ server/controllers/flagsControllers.js
@@ -6,74 +6,125 @@ import comments from '../models/commentsModels';
 
 
 class flagsController {
-  // flag an article
   static async flagArticle(req, res) {
-    const { error } = validateArticle(req.body);
-    if (error) { return response.response(res, 422, 'error', `${error.details[0].message}`, true); }
+    try {
+      const { id: userId } = req.user;
+      const { error } = validateArticle(req.body);
+      if (error) { return response.response(res, 422, 'error', `${error.details[0].message}`, true); }
 
-    const { id } = req.params;
+      const { id } = req.params;
+      const findowner = await articles.filter(
+        (article) => article.articleId === parseInt(id, 10) && article.authorId === userId,
+      );
+      if (findowner.length > 0) {
+        return response.response(res, 403, 'error', 'you can not flag your own article  ', true);
+      }
 
-    const findarticle = await articles.filter(
-      (article) => article.articleId === parseInt(id, 10),
-    );
+      const findarticle = await articles.filter(
+        (article) => article.articleId === parseInt(id, 10),
+      );
 
-    if (findarticle.length > 0) {
-      const { article } = findarticle[0];
-      const { reason: motif } = req.body;
-      const data = {
-        flagId: flags.length + 1,
-        type: 'article',
-        flagedId: parseInt(id, 10),
-        content: article,
-        reason: motif,
-      };
-      flags.push(data);
-      response.response(res, 201, 'report send successfully', data, false);
-    } else { return response.response(res, 404, 'error', 'article Not Found  ', true); }
-    return (findarticle);
+      if (findarticle.length > 0) {
+        const { article } = findarticle[0];
+        const { reason: motif } = req.body;
+
+        const arledyFlaged = await flags.filter(
+          (flag) => flag.flagedId === parseInt(id, 10) && flag.reason === motif,
+        );
+        if (arledyFlaged.length > 0) {
+          return response.response(res, 409, 'error', 'article arleady flagged with the same reason  ', true);
+        }
+        const data = {
+          flagId: flags.length + 1,
+          type: 'article',
+          flagedId: parseInt(id, 10),
+          content: article,
+          reason: motif,
+        };
+        flags.push(data);
+        response.response(res, 201, 'report send successfully', data, false);
+      } else { return response.response(res, 404, 'error', 'article Not Found  ', true); }
+      return (findarticle);
+    } catch (error) {
+      return error;
+    }
   }
 
   // flag a comment
   static async flagComment(req, res) {
-    const { error } = validateArticle(req.body);
-    if (error) { return response.response(res, 422, 'error', `${error.details[0].message}`, true); }
+    try {
+      const { id: userId } = req.user;
+      const { error } = validateArticle(req.body);
+      if (error) { return response.response(res, 422, 'error', `${error.details[0].message}`, true); }
 
-    const { id } = req.params;
+      const { id } = req.params;
 
-    const findcomment = await comments.filter(
-      (comment) => comment.commentId === parseInt(id, 10),
-    );
+      const findowner = await comments.filter(
+        (comment) => comment.commentId === parseInt(id, 10) && comment.authorId === userId,
+      );
+      if (findowner.length > 0) {
+        return response.response(res, 403, 'error', 'you can not flag your comment  ', true);
+      }
 
-    if (findcomment.length > 0) {
-      const { comment } = findcomment[0];
-      const { reason: motif } = req.body;
-      const data = {
-        flagId: flags.length + 1,
-        type: 'comment',
-        flagedId: parseInt(id, 10),
-        content: comment,
-        reason: motif,
-      };
-      flags.push(data);
-      response.response(res, 201, 'report send successfully', data, false);
-    } else { return response.response(res, 404, 'error', 'comment Not Found  ', true); }
-    return (findcomment);
+      const findcomment = await comments.filter(
+        (comment) => comment.commentId === parseInt(id, 10),
+      );
+
+      if (findcomment.length > 0) {
+        const { comment } = findcomment[0];
+        const { reason: motif } = req.body;
+        const arledyFlaged = await flags.filter(
+          (flag) => flag.flagedId === parseInt(id, 10) && flag.reason === motif,
+        );
+        if (arledyFlaged.length > 0) {
+          return response.response(res, 409, 'error', 'Comment arleady flagged with the same reason  ', true);
+        }
+        const data = {
+          flagId: flags.length + 1,
+          type: 'comment',
+          flagedId: parseInt(id, 10),
+          content: comment,
+          reason: motif,
+        };
+        flags.push(data);
+        response.response(res, 201, 'report send successfully', data, false);
+      } else { return response.response(res, 404, 'error', 'comment Not Found  ', true); }
+      return (findcomment);
+    } catch (error) {
+      return error;
+    }
   }
 
   static async getFlags(req, res) {
-    return response.response(res, 200, 'success', flags, false);
+    try {
+      return response.response(res, 200, 'Listes Of flagged items', flags, false);
+    } catch (error) {
+      return error;
+    }
   }
 
   static async deleteComment(req, res) {
-    const { id } = req.params;
-    const findComment = await comments.findIndex(
-      (findComments) => findComments.commentId === parseInt(id, 10),
-    );
-    if (findComment !== -1) {
-      comments.splice(findComment, 1);
-      response.response(res, 200, 'comment successfully deleted');
-    } else { return response.response(res, 404, 'error', 'comment Not Found  ', true); }
-    return (findComment);
+    try {
+      const { id } = req.params;
+      const findComment = await comments.findIndex(
+        (findComments) => findComments.commentId === parseInt(id, 10),
+      );
+      const findflag = await flags.findIndex(
+        (findflags) => findflags.flagedId === parseInt(id, 10) && findflags.type === 'comment',
+      );
+
+      if (findComment !== -1) {
+        if (findflag === -1) {
+          return response.response(res, 403, 'error', 'comment not flagged  ', true);
+        }
+        comments.splice(findComment, 1);
+        flags.splice(findflag, 1);
+        response.response(res, 200, 'comment successfully deleted');
+      } else { return response.response(res, 404, 'error', 'comment Not Found  ', true); }
+      return (findComment);
+    } catch (error) {
+      return error;
+    }
   }
 }
 export default flagsController;
