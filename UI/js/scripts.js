22fbe671ec9312ec0f18d9c78beb098d5476b785
Believe <niyeric11@.com>
2019-10-03 17:47:48 +0200

    updating UI

--- UI/js/scripts.js
+++ UI/js/scripts.js
@@ -1,11 +1,9 @@
 /* eslint-disable no-unused-vars */
-// switch between auth
 const switchbtnauth = () => {
   const authFormElements = document.getElementsByClassName('auth-area');
   Array.from(authFormElements).forEach((form) => form.classList.toggle('form-hidden'));
 };
 
-// Side Navigation
 const openNav = () => {
   document.getElementById('mySidenav').style.width = '250px';
 };
@@ -14,18 +12,15 @@ const closeNav = () => {
   document.getElementById('mySidenav').style.width = '0';
 };
 const dialogbox = (message) => {
-  // Get the modal
   const modal = document.querySelector('#dialogbox');
-
   const divMsg = document.querySelector('.dialog-content-js');
 
   divMsg.textContent = message;
-  // Display the modal
   modal.style.display = 'block';
 };
-// close message dialog box
 const closeDialog = () => {
   document.querySelector('.modal').style.display = 'none';
+  document.querySelector('#flagreason').style.display = 'none';
   document.querySelector('.comment-form').style.display = 'none';
   document.querySelector('.mycomment-form').style.display = 'none';
   document.querySelector('.new-article').style.display = 'none';
@@ -36,34 +31,28 @@ const closeDialog = () => {
 };
 
 const post = () => {
-  // Get the comments
   const posts = document.querySelector('.comment-form');
 
   posts.style.display = 'block';
 };
 
 const mypost = () => {
-  // Get the comments
   const myposts = document.querySelector('.mycomment-form');
 
   myposts.style.display = 'block';
 };
 const newArticle = () => {
-  // post new article
   const posts = document.querySelector('.new-article');
 
   posts.style.display = 'block';
 };
 const updateArticle = () => {
-  // update  article
   const posts = document.querySelector('.update-article');
 
   posts.style.display = 'block';
 };
 
-// Get the listes
 const openArticles = () => {
-  // hide
   const manFlags = document.querySelector('.all-flags');
   manFlags.style.display = 'none';
   const manUsers = document.querySelector('.all-users');
@@ -78,7 +67,6 @@ const openArticles = () => {
   const adminaccount = document.querySelector('.menulink-admin4');
   adminaccount.style.border = ' 3px solid var(--default-link-text-color)';
 
-  // display
   const adminarticles = document.querySelector('.menulink-admin1');
   adminarticles.style.border = ' 3px solid var(--color-primary)';
 
@@ -86,9 +74,7 @@ const openArticles = () => {
   manArticles.style.display = 'block';
 };
 
-// open liste flags
 const openFlags = () => {
-  // Get the comments
   const manArticles = document.querySelector('.all-articles');
   manArticles.style.display = 'none';
   const manUsers = document.querySelector('.all-users');
@@ -103,14 +89,12 @@ const openFlags = () => {
   const adminaccount = document.querySelector('.menulink-admin4');
   adminaccount.style.border = ' 3px solid var(--default-link-text-color)';
 
-  // display
   const manFlags = document.querySelector('.all-flags');
   manFlags.style.display = 'block';
   const adminflags = document.querySelector('.menulink-admin2');
   adminflags.style.border = ' 3px solid var(--color-primary)';
 };
 
-// open liste users
 const openUsers = () => {
   const manArticles = document.querySelector('.all-articles');
   manArticles.style.display = 'none';
@@ -126,41 +110,16 @@ const openUsers = () => {
   const adminaccount = document.querySelector('.menulink-admin4');
   adminaccount.style.border = ' 3px solid var(--default-link-text-color)';
 
-  // // display
   const manUsers = document.querySelector('.all-users');
   manUsers.style.display = 'block';
 
   const adminusers = document.querySelector('.menulink-admin3');
   adminusers.style.border = ' 3px solid var(--color-primary)';
 };
-
-// open admin account
-const openAccount = () => {
-  const manArticles = document.querySelector('.all-articles');
-  manArticles.style.display = 'none';
-  const manFlags = document.querySelector('.all-flags');
-  manFlags.style.display = 'none';
-  const manUsers = document.querySelector('.all-users');
-  manUsers.style.display = 'none';
-
-  const adminarticles = document.querySelector('.menulink-admin1');
-  adminarticles.style.border = ' 3px solid var(--default-link-text-color)';
-  const adminflags = document.querySelector('.menulink-admin2');
-  adminflags.style.border = ' 3px solid var(--default-link-text-color)';
-  const adminusers = document.querySelector('.menulink-admin3');
-  adminusers.style.border = ' 3px solid var(--default-link-text-color)';
-
-  // display
-  const manaccount = document.querySelector('.AdminForm-wrapper');
-  manaccount.style.display = 'block';
-
-  const adminaccount = document.querySelector('.menulink-admin4');
-  adminaccount.style.border = ' 3px solid var(--color-primary)';
-};
-
-// flag an article or Comment
 const flag = () => {
+  const flagreason = document.querySelector('#flagreason');
   const reportflag = document.querySelector('.report');
+  flagreason.style.display = 'block';
   if (reportflag.style.color === 'red') {
     reportflag.style.color = 'black  ';
   } else {
@@ -168,15 +127,9 @@ const flag = () => {
   }
 };
 const flag2 = () => {
+  const flagreason = document.querySelector('#flagreason');
+  flagreason.style.display = 'block';
   const reportflag = document.querySelector('.report2');
-  if (reportflag.style.color === 'red') {
-    reportflag.style.color = 'black  ';
-  } else {
-    reportflag.style.color = 'red';
-  }
-};
-const flag3 = () => {
-  const reportflag = document.querySelector('.report3');
   if (reportflag.style.color === 'red') {
     reportflag.style.color = 'black  ';
   } else {
@@ -185,6 +138,8 @@ const flag3 = () => {
 };
 
 const flag5 = () => {
+  const flagreason = document.querySelector('#flagreason');
+  flagreason.style.display = 'block';
   const reportflag = document.querySelector('.report5');
   if (reportflag.style.color === 'red') {
     reportflag.style.color = 'black  ';
