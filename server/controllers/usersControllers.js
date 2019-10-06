319625f7662fa49a1f8d319393aa2a057a29cee7
Believe <niyeric11@.com>
2019-10-06 19:58:19 +0200

    feature(API): user signup [finishes #168972142]

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -1,80 +1,79 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import { Client } from 'pg';
+import dotenv from 'dotenv';
 import validateUser from '../middlewares/validateUser';
 import validateLogin from '../middlewares/validateLogin';
 import users from '../models/usersModels';
 import response from '../helpers/response';
 
+dotenv.config();
+const { DATABASE_URL } = process.env;
+const connectionString = DATABASE_URL;
+const client = new Client({
+  connectionString,
+});
+client.connect();
+
 class usersController {
   static async regUser(req, res) {
-    try {
-      const { error } = validateUser(req.body);
+    const { error } = validateUser(req.body);
 
-      if (error) {
-        return response.response(
-          res,
-          422,
-          'error',
-          `${error.details[0].message}`,
-          true,
-        );
-      }
-
-      const user = await users.filter(
-        (usermail) => usermail.email.toLowerCase() === req.body.email.toLowerCase(),
+    if (error) {
+      return response.response(
+        res,
+        422,
+        'error',
+        `${error.details[0].message}`,
+        true,
       );
-      if (user.length > 0) {
-        response.response(res, 409, 'error', 'User with that email already registered', true);
-      } else {
-        const {
-          firstName,
-          lastName,
-          email,
-          password,
-          gender,
-          jobRole,
-          department,
-          address,
-        } = req.body;
-        // Add to object
-        const addUser = {
-          id: users.length + 1,
-          firstName,
-          lastName,
-          email,
-          password,
-          gender,
-          jobRole,
-          department,
-          address,
-          isAdmin: false,
-        };
-        const salt = await bcrypt.genSalt(10);
-        addUser.password = await bcrypt.hash(addUser.password, salt);
-        users.push(addUser);
-        const hideitems = { ...addUser };
-        delete hideitems.password;
-
-        const useremail = await users.filter(
-          (usermail) => usermail.email.toLowerCase() === req.body.email.toLowerCase(),
-        );
-        const token = jwt.sign(
-          { id: useremail[0].id, isAdmin: useremail[0].isAdmin },
-          process.env.JWT,
-        );
-        const data = { token };
-        return response.response(
-          res,
-          201,
-          'user registered successfully',
-          data,
-          false,
-        );
-      }
-      return response;
-    } catch (error) {
-      return error;
     }
+
+    const user = await client.query('SELECT * FROM users WHERE email=$1 ', [req.body.email.toLowerCase()]);
+    if (user.rows.length > 0) {
+      response.response(res, 409, 'error', 'User with that email already registered', true);
+    } else {
+      const {
+        firstName,
+        lastName,
+        email,
+        password,
+        gender,
+        jobRole,
+        department,
+        address,
+      } = req.body;
+      // Add to object
+      let admin = 'false';
+      if (req.body.email === process.env.administrator) {
+        admin = 'true';
+      }
+      const salt = await bcrypt.genSalt(10);
+      const newpassword = await bcrypt.hash(password, salt);
+      client.query('INSERT INTO users(first_name, last_name, email, password, gender, job_role, department,address,is_admin)VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9)', [
+        firstName,
+        lastName,
+        email.toLowerCase(), newpassword,
+        gender.toUpperCase(), jobRole, department, address, admin,
+      ]);
+
+      const useremail = await client.query('SELECT * FROM users WHERE email=$1 ', [
+        req.body.email.toLowerCase(),
+      ]);
+      const token = jwt.sign(
+        { id: useremail.rows[0].id, isAdmin: useremail.rows[0].is_admin },
+        process.env.JWT,
+      );
+      const data = { token };
+      return response.response(
+        res,
+        201,
+        'user registered successfully',
+        data,
+        false,
+      );
+    }
+    return response;
   }
 
   static async loginUser(req, res) {
